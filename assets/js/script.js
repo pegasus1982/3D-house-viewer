c021f42c57d16f761d2661a602a52f3e96fdf6db
pegasus1982 <gdm19920108@gmail.com>
2019-03-04 04:37:19 +0800

    add seperated floor plans, add hover floor plan event

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,32 +8,89 @@ var scene,
     camera,
     light1,
     light2,
+    light3,
     ground;
 
 var advancedTexture;
 
 var modelHouse;
-
+var planList = [
+    {
+        name : "PLAN_FLOOR_02_ROOM_001",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_002",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_003",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_004",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_005",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_006",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_007",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_008",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_009",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_010",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_011",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_012",
+        target : ""
+    },
+    {
+        name : "PLAN_FLOOR_02_ROOM_013",
+        target : ""
+    },
+];
 var createScene = function(){
     scene = new BABYLON.Scene(engine);
     
-    camera = new BABYLON.ArcRotateCamera("Camera", -0.36, 1.53, 1500, new BABYLON.Vector3(0,40,0), scene);
+    camera = new BABYLON.ArcRotateCamera("Camera", -0.36, 1.53, 1500, new BABYLON.Vector3(0,50,0), scene);
     camera.attachControl(canvas, false);
-    camera.upperBetaLimit = 1.58;
+    camera.upperBetaLimit = 1.57;
     // camera.lowerBetaLimit = 0.4;
-    camera.wheelPrecision = 0.1;
+    camera.wheelPrecision = 0.2;
     
     camera.upperRadiusLimit = 2000;
     camera.lowerRadiusLimit = 500;
 
-    light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(4000, 6000, 5000), scene);
+    // light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(4000, 6000, 5000), scene);
     light2 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(-4000, 7000, -6000), scene);
+    light3 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(4000, 2000, 900), scene);
 
+    var shadowGenerator = new BABYLON.ShadowGenerator(1024,light3);
     var assetsManager = new BABYLON.AssetsManager(scene);
 
     // load house model
     var meshTask1 = assetsManager.addMeshTask("model task", "", "assets/models/house/", "house.babylon");
     meshTask1.onSuccess = function (task) {
+        console.log(task.loadedMeshes)
         for(var i in task.loadedMeshes){
             if(task.loadedMeshes[i].name == "PLAN_FLOOR_01" || task.loadedMeshes[i].name == "PLAN_FLOOR_02")
                 task.loadedMeshes[i].visibility = false;
@@ -48,6 +105,13 @@ var createScene = function(){
 
     assetsManager.load();
 
+    // var box1 = new BABYLON.Mesh.CreateBox("box 1",40,scene,false);
+    // box1.position = new BABYLON.Vector3(100,100,100)
+    // var shadowGenerator1 = new BABYLON.ShadowGenerator(512,light3);
+    // shadowGenerator1.getShadowMap().renderList.push(box1);
+    // shadowGenerator1.useBlurExponentialShadowMap = true;
+    // shadowGenerator1.blurBoxOffset = 2.0;
+
     //generate ground
     var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "assets/texture/heightMap-01.png", 6000, 6000, 30, 0, 500, scene, false);
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
@@ -56,74 +120,72 @@ var createScene = function(){
     groundMaterial.diffuseTexture.uScale = 40;
     groundMaterial.diffuseTexture.vScale = 40;
     groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    groundMaterial.emissiveColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+    // groundMaterial.emissiveColor = new BABYLON.Color3(0.3, 0.3, 0.3);
     ground.material = groundMaterial;
     ground.receiveShadows = true;
     ground.checkCollisions = true;
 
-    ground.onReady = function(){
-        ground.optimize(100);
+    // ground.onReady = function(){
+    //     ground.optimize(100);
 
-        var shadowGenerator = new BABYLON.ShadowGenerator(1024,light1);
+    //     BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","tree-01.babylon",scene, function(newMeshes){
+    //         newMeshes[0].material.opacityTexture = null;
+    //         newMeshes[0].material.backFaceCulling = false;
+    //         newMeshes[0].isVisible = false;
+    //         newMeshes[0].position.y = ground.getHeightAtCoordinates(0, 0); // Getting height from ground object
 
-        BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","tree-01.babylon",scene, function(newMeshes){
-            newMeshes[0].material.opacityTexture = null;
-            newMeshes[0].material.backFaceCulling = false;
-            newMeshes[0].isVisible = false;
-            newMeshes[0].position.y = ground.getHeightAtCoordinates(0, 0); // Getting height from ground object
+    //         shadowGenerator.getShadowMap().renderList.push(newMeshes[0]);
+    //         var treeAxis = [
+    //             [2000,  2000,   0],
+    //             [-2000, 2000,   100],
+    //             [-1800, 2300,   200],
+    //             [-2500, 1800,   240],
+    //             [-2000, 1600,   100],
+    //             [-2500, 1400,   100],
+    //             [-1000, 1000,   0],
+    //             [-1500, 400,    0],
+    //             [-1200, 0,      0],
+    //             [-1500, -300,   0],
+    //             [-2500, 0,      0],
+    //             [-2000, -300,   0],
+    //             [-2200, -900,   0],
+    //             [-1600, -1400,  0],
+    //             [0,     1200,   0],
+    //             [400,   1200,   0],
+    //             [800,   1200,   0],
+    //             [-200,  1600,   0],
+    //             [200,   2000,   0],
+    //             [600,   1800,   0],
+    //             [0,     -800,   0],
+    //             [400,   -1100,  0],
+    //             [800,   -1400,  0],
+    //             [-900,  -1300,  0],
+    //             [-600,  -1100,  0],
+    //             [-800,  -1900,  0],
+    //         ];
+    //         for (var index = 0; index < treeAxis.length; index++) {
+    //             var newInstance = newMeshes[0].createInstance("i" + index);
+    //             var x = treeAxis[index][0];
+    //             var z = treeAxis[index][1];
+    //             var y = treeAxis[index][2];
 
-            // shadowGenerator.getShadowMap().renderList.push(newMeshes[0]);
-            var treeAxis = [
-                [2000,  2000,   0],
-                [-2000, 2000,   100],
-                [-1800, 2300,   200],
-                [-2500, 1800,   240],
-                [-2000, 1600,   100],
-                [-2500, 1400,   100],
-                [-1000, 1000,   0],
-                [-1500, 400,    0],
-                [-1200, 0,      0],
-                [-1500, -300,   0],
-                [-2500, 0,      0],
-                [-2000, -300,   0],
-                [-2200, -900,   0],
-                [-1600, -1400,  0],
-                [0,     1200,   0],
-                [400,   1200,   0],
-                [800,   1200,   0],
-                [-200,  1600,   0],
-                [200,   2000,   0],
-                [600,   1800,   0],
-                [0,     -800,   0],
-                [400,   -1100,  0],
-                [800,   -1400,  0],
-                [-900,  -1300,  0],
-                [-600,  -1100,  0],
-                [-800,  -1900,  0],
-            ];
-            for (var index = 0; index < treeAxis.length; index++) {
-                var newInstance = newMeshes[0].createInstance("i" + index);
-                var x = treeAxis[index][0];
-                var z = treeAxis[index][1];
-                var y = treeAxis[index][2];
+    //             newInstance.position = new BABYLON.Vector3(x, y, z);
 
-                newInstance.position = new BABYLON.Vector3(x, y, z);
+    //             newInstance.rotate(BABYLON.Axis.Y, Math.random() * Math.PI * 2, BABYLON.Space.WORLD);
 
-                newInstance.rotate(BABYLON.Axis.Y, Math.random() * Math.PI * 2, BABYLON.Space.WORLD);
+    //             var scale = 0.5 + Math.random() * 2;
+    //             newInstance.scaling.addInPlace(new BABYLON.Vector3(scale, scale, scale));
 
-                var scale = 0.5 + Math.random() * 2;
-                newInstance.scaling.addInPlace(new BABYLON.Vector3(scale, scale, scale));
+    //             shadowGenerator.getShadowMap().renderList.push(newInstance);
+    //         }
+    //         shadowGenerator.getShadowMap().refreshRate = 0; // We need to compute it just once
+    //         shadowGenerator.usePoissonSampling = true;
 
-                // shadowGenerator.getShadowMap().renderList.push(newInstance);
-            }
-            shadowGenerator.getShadowMap().refreshRate = 0; // We need to compute it just once
-            shadowGenerator.usePoissonSampling = true;
-
-            // Collisions
-            camera.checkCollisions = true;
-            camera.applyGravity = true;
-        })
-    }
+    //         // Collisions
+    //         camera.checkCollisions = true;
+    //         camera.applyGravity = true;
+    //     })
+    // }
 
     //generate skybox
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 12000.0, scene);
@@ -164,12 +226,12 @@ function hideFloor1(){
             modelHouse[i].visibility = false;
         else if(modelName.includes('BEAM_F1_'))
             modelHouse[i].visibility = false;
-        else if(modelName.includes('DOOR_F1_'))
-            modelHouse[i].visibility = false;
+        // else if(modelName.includes('DOOR_F1_'))
+            // modelHouse[i].visibility = false;
         else if(modelName.includes('CEIL_GF_'))
             modelHouse[i].visibility = false;
-        else if(modelName.includes('WIND_F1_'))
-            modelHouse[i].visibility = false;
+        // else if(modelName.includes('WIND_F1_'))
+            // modelHouse[i].visibility = false;
         else if(modelName == 'CEIL_F1_FOUNDATIONS_CONCEPT_WHITE')
             modelHouse[i].visibility = false;
         else if(modelName == 'MESH_GF_GROUND_FLOOR_CONCRETE___GENERAL')
@@ -247,9 +309,9 @@ function hideFloor1Base(){
         else if(modelName == 'CEIL_F1_FOUNDATIONS_CONCRETE___GENERAL')
             modelHouse[i].visibility = true;
 
-        if(modelName == "PLAN_FLOOR_01")
+        if(modelName.includes("PLAN_FLOOR_01"))
             modelHouse[i].visibility = true;
-        if(modelName == "PLAN_FLOOR_02")
+        if(modelName.includes("PLAN_FLOOR_02"))
             modelHouse[i].visibility = false;
     }
 }
@@ -264,10 +326,10 @@ function hideFloor2(){
             modelHouse[i].visibility = false;
         else if(modelName.includes('BEAM_GF_'))
             modelHouse[i].visibility = false;
-        else if(modelName.includes('DOOR_GF_'))
-            modelHouse[i].visibility = false;
-        else if(modelName.includes('WIND_GF_'))
-            modelHouse[i].visibility = false;
+        // else if(modelName.includes('DOOR_GF_'))
+        //     modelHouse[i].visibility = false;
+        // else if(modelName.includes('WIND_GF_'))
+            // modelHouse[i].visibility = false;
         else if(modelName.includes('COLU_GF_'))
             modelHouse[i].visibility = false;
     }
@@ -299,18 +361,29 @@ function hideFloor2Base(){
         else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM62_0133_GRAY')
             modelHouse[i].visibility = false;
 
-        if(modelName == "PLAN_FLOOR_02")
+        if(modelName.includes("PLAN_FLOOR_02"))
             modelHouse[i].visibility = true;
     }
 }
 
 //add floor select event
+function loadPlan(){
+    for(var i in modelHouse){
+        if(modelHouse[i].name.includes("PLAN_FLOOR_01") || modelHouse[i].name.includes("PLAN_FLOOR_02")){
+            modelHouse[i].material = new BABYLON.StandardMaterial(modelHouse[i].name+'_material', scene);
+            modelHouse[i].material.diffuseTexture = new BABYLON.Texture(modelHouse[i].name.includes('PLAN_FLOOR_01')?"assets/texture/plans/floor-plan-1.png":"assets/texture/plans/floor-plan-2.png", scene);
+            modelHouse[i].material.diffuseTexture.vOffset = 200;
+            modelHouse[i].material.diffuseTexture.uScale = 0.0016;
+            modelHouse[i].material.diffuseTexture.vScale = 0.0016;
+        }
+    }
+}
 $('.select-floor').click(function(){
     var numFloor = $(this).attr('floor');
     numFloor = parseInt(numFloor);
 
     for(var i in modelHouse){
-        if(modelHouse[i].name == "PLAN_FLOOR_01" || modelHouse[i].name == "PLAN_FLOOR_02"){
+        if(modelHouse[i].name.includes("PLAN_FLOOR_01") || modelHouse[i].name.includes("PLAN_FLOOR_02")){
             modelHouse[i].visibility = false;
         }
         else{
@@ -318,7 +391,8 @@ $('.select-floor').click(function(){
         }
         
     }
-    //in case of floor 1 selected
+    loadPlan();
+
     if(numFloor == 1){
         hideFloor2Base();
         hideFloor1();
@@ -336,3 +410,68 @@ $('.select-floor').click(function(){
         hideFloor2Base();
     }
 })
+
+//mouse move event
+var current = null;
+function onPointerMove(evt){
+    var pickInfo = scene.pick(scene.pointerX, scene.pointerY,function(mesh){
+        return (mesh.visibility && (mesh.name.includes("PLAN_FLOOR_01") || mesh.name.includes("PLAN_FLOOR_02")))
+    })
+    if(pickInfo.hit){
+        var currentMesh = pickInfo.pickedMesh;
+        var isPlan = false;
+        for(var i in planList){
+            if(currentMesh.visibility && currentMesh.name == planList[i].name){
+                isPlan = true;
+                break;
+            }
+        }
+        if(isPlan){
+            if(current !== currentMesh){
+                current = currentMesh;
+            }
+            else return;
+
+            loadPlan();
+            var random = Math.floor(Math.random()*10)%3 + 1;
+            currentMesh.material = new BABYLON.StandardMaterial(currentMesh.name+random, scene);
+            currentMesh.material.diffuseTexture = new BABYLON.Texture("assets/texture/plans/floor-plan-2-random-color-"+random+".png", scene);
+            currentMesh.material.diffuseTexture.uScale = 0.0016;
+            currentMesh.material.diffuseTexture.vScale = 0.0016;
+        }
+    }
+}
+
+function showModal(){
+    $('body').append(
+                '<div class="modal-container">'+
+                '<div class="modal-content"></div>'+
+                '</div>'
+            );
+}
+function onPointerDown(evt){
+    var pickInfo = scene.pick(scene.pointerX, scene.pointerY,function(mesh){
+        return (mesh.visibility && (mesh.name.includes("PLAN_FLOOR_01") || mesh.name.includes("PLAN_FLOOR_02")))
+    })
+    if(pickInfo.hit){
+        var currentMesh = pickInfo.pickedMesh;
+        console.log(currentMesh.name)
+        var isPlan = false;
+        var target = null;
+        for(var i in planList){
+            if(currentMesh.visibility && currentMesh.name == planList[i].name){
+                isPlan = true;
+                target = planList[i].target;
+                break;
+            }
+        }
+        if(isPlan){
+            
+        }
+    }
+}
+
+var canvas = engine.getRenderingCanvas();
+canvas.addEventListener('pointermove',onPointerMove);
+canvas.addEventListener('pointerdown',onPointerDown);
+showModal();
