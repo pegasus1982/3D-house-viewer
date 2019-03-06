12c45887eb1bf2d7f0f77ae2a51cd53855269db0
pegasus1982 <gdm19920108@gmail.com>
2019-03-06 15:44:07 +0800

    update plans

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,129 +14,129 @@ var scene,
 var advancedTexture;
 
 var modelHouse;
-var planList = [
-    {
-        name : "PLAN_FLOOR_02_ROOM_001",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_002",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_003",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_004",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_005",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_006",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_007",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_008",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_009",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_010",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_011",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_012",
-        target : "floor-02-room-02.png"
-    },
-    {
-        name : "PLAN_FLOOR_02_ROOM_013",
-        target : "floor-02-room-02.png"
-    },
+// var planList = [
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_001",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_002",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_003",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_004",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_005",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_006",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_007",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_008",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_009",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_010",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_011",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_012",
+//         target : "floor-02-room-02.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_02_ROOM_013",
+//         target : "floor-02-room-02.png"
+//     },
 
-    {
-        name : "PLAN_FLOOR_01_ROOM_001",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_002",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_003",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_004",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_005",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_006",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_007",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_008",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_009",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_010",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_011",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_012",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_013",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_014",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_015",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_016",
-        target : "floor-02-room-01.png"
-    },
-    {
-        name : "PLAN_FLOOR_01_ROOM_017",
-        target : "floor-02-room-01.png"
-    },
-];
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_001",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_002",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_003",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_004",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_005",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_006",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_007",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_008",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_009",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_010",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_011",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_012",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_013",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_014",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_015",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_016",
+//         target : "floor-02-room-01.png"
+//     },
+//     {
+//         name : "PLAN_FLOOR_01_ROOM_017",
+//         target : "floor-02-room-01.png"
+//     },
+// ];
 
 var cameraSticker1,
     cameraSticker2,
@@ -147,7 +147,7 @@ var universalCamera1,
     universalCamera2,
     universalCamera3,
     universalCamera4;
-
+var hl;
 var createScene = function(){
     scene = new BABYLON.Scene(engine);
     
@@ -164,6 +164,10 @@ var createScene = function(){
     light2 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(-4000, 7000, -6000), scene);
     light3 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(4000, 2000, -900), scene);
 
+    hl = new BABYLON.HighlightLayer("hl", scene);
+    hl.blurHorizontalSize   = 2;
+    hl.blurVerticalSize     = 2;
+
     var shadowGenerator = new BABYLON.ShadowGenerator(1024,light3);
     var assetsManager = new BABYLON.AssetsManager(scene);
 
@@ -173,6 +177,7 @@ var createScene = function(){
         for(var i in task.loadedMeshes){
             if(task.loadedMeshes[i].name.includes("PLAN_FLOOR_01") || task.loadedMeshes[i].name.includes("PLAN_FLOOR_02")){
                 task.loadedMeshes[i].visibility = false;
+                hl.addMesh(task.loadedMeshes[i], BABYLON.Color3.Green());
             }
             task.loadedMeshes[i].position.y -= 35;
         }
@@ -190,7 +195,7 @@ var createScene = function(){
     meshTask1.onError = function(task,message,exception){
         console.log(task,message,exception);
     }
-
+    
     assetsManager.load();
 
     //generate ground
@@ -205,68 +210,68 @@ var createScene = function(){
     ground.receiveShadows = true;
     ground.checkCollisions = true;
 
-    ground.onReady = function(){
-        ground.optimize(100);
+    // ground.onReady = function(){
+    //     ground.optimize(100);
 
-        //scatter trees
-        BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","tree-01.babylon",scene, function(newMeshes){
-            newMeshes[0].material.opacityTexture = null;
-            newMeshes[0].material.backFaceCulling = false;
-            newMeshes[0].isVisible = false;
-            newMeshes[0].position.y = ground.getHeightAtCoordinates(0, 0); // Getting height from ground object
+    //     //scatter trees
+    //     BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","tree-01.babylon",scene, function(newMeshes){
+    //         newMeshes[0].material.opacityTexture = null;
+    //         newMeshes[0].material.backFaceCulling = false;
+    //         newMeshes[0].isVisible = false;
+    //         newMeshes[0].position.y = ground.getHeightAtCoordinates(0, 0); // Getting height from ground object
 
-            shadowGenerator.getShadowMap().renderList.push(newMeshes[0]);
-            var treeAxis = [
-                [2000,  2000,   0],
-                [-2000, 2000,   100],
-                [-1800, 2300,   200],
-                [-2500, 1800,   240],
-                [-2000, 1600,   100],
-                [-2500, 1400,   100],
-                [-1000, 1000,   0],
-                [-1500, 400,    0],
-                [-1200, 0,      0],
-                [-1500, -300,   0],
-                [-2500, 0,      0],
-                [-2000, -300,   0],
-                [-2200, -900,   0],
-                [-1600, -1400,  0],
-                [0,     1200,   0],
-                [400,   1200,   0],
-                [800,   1200,   0],
-                [-200,  1600,   0],
-                [200,   2000,   0],
-                [600,   1800,   0],
-                [0,     -800,   0],
-                [400,   -1100,  0],
-                [800,   -1400,  0],
-                [-900,  -1300,  0],
-                [-600,  -1100,  0],
-                [-800,  -1900,  0],
-            ];
-            for (var index = 0; index < treeAxis.length; index++) {
-                var newInstance = newMeshes[0].createInstance("i" + index);
-                var x = treeAxis[index][0];
-                var z = treeAxis[index][1];
-                var y = treeAxis[index][2];
+    //         shadowGenerator.getShadowMap().renderList.push(newMeshes[0]);
+    //         var treeAxis = [
+    //             [2000,  2000,   0],
+    //             [-2000, 2000,   100],
+    //             [-1800, 2300,   200],
+    //             [-2500, 1800,   240],
+    //             [-2000, 1600,   100],
+    //             [-2500, 1400,   100],
+    //             [-1000, 1000,   0],
+    //             [-1500, 400,    0],
+    //             [-1200, 0,      0],
+    //             [-1500, -300,   0],
+    //             [-2500, 0,      0],
+    //             [-2000, -300,   0],
+    //             [-2200, -900,   0],
+    //             [-1600, -1400,  0],
+    //             [0,     1200,   0],
+    //             [400,   1200,   0],
+    //             [800,   1200,   0],
+    //             [-200,  1600,   0],
+    //             [200,   2000,   0],
+    //             [600,   1800,   0],
+    //             [0,     -800,   0],
+    //             [400,   -1100,  0],
+    //             [800,   -1400,  0],
+    //             [-900,  -1300,  0],
+    //             [-600,  -1100,  0],
+    //             [-800,  -1900,  0],
+    //         ];
+    //         for (var index = 0; index < treeAxis.length; index++) {
+    //             var newInstance = newMeshes[0].createInstance("i" + index);
+    //             var x = treeAxis[index][0];
+    //             var z = treeAxis[index][1];
+    //             var y = treeAxis[index][2];
 
-                newInstance.position = new BABYLON.Vector3(x, y, z);
+    //             newInstance.position = new BABYLON.Vector3(x, y, z);
 
-                newInstance.rotate(BABYLON.Axis.Y, Math.random() * Math.PI * 2, BABYLON.Space.WORLD);
+    //             newInstance.rotate(BABYLON.Axis.Y, Math.random() * Math.PI * 2, BABYLON.Space.WORLD);
 
-                var scale = 0.5 + Math.random() * 2;
-                newInstance.scaling.addInPlace(new BABYLON.Vector3(scale, scale, scale));
+    //             var scale = 0.5 + Math.random() * 2;
+    //             newInstance.scaling.addInPlace(new BABYLON.Vector3(scale, scale, scale));
 
-                shadowGenerator.getShadowMap().renderList.push(newInstance);
-            }
-            shadowGenerator.getShadowMap().refreshRate = 0;
-            shadowGenerator.usePoissonSampling = true;
+    //             shadowGenerator.getShadowMap().renderList.push(newInstance);
+    //         }
+    //         shadowGenerator.getShadowMap().refreshRate = 0;
+    //         shadowGenerator.usePoissonSampling = true;
 
-            // Collisions
-            camera.checkCollisions = true;
-            camera.applyGravity = true;
-        });
-    }
+    //         // Collisions
+    //         camera.checkCollisions = true;
+    //         camera.applyGravity = true;
+    //     });
+    // }
 
     //generate skybox
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 10000.0, scene);
@@ -350,11 +355,11 @@ var createScene = function(){
     });
 
     //import garden
-    BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","garden.babylon",scene, function(newMeshes){
-        for(var i in newMeshes){
-            newMeshes[i].position.z += 19;
-        }
-    });
+    // BABYLON.SceneLoader.ImportMesh("","assets/models/terrain/","garden.babylon",scene, function(newMeshes){
+    //     for(var i in newMeshes){
+    //         newMeshes[i].position.z += 19;
+    //     }
+    // });
     return scene;
 }
 
@@ -512,6 +517,26 @@ function hideFloor2Base(){
             modelHouse[i].visibility = false;
         else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM62_0133_GRAY')
             modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDML53__0136_CHARCOAL_1_FLAT_PART')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM54_DEFAULT')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM56__AUTO_2_38')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM58__COLOR_F17_2')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM59_PDM_GLASS_SIDEBOARD_0')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM60_PDM_STEEL_BRUSHED_54_FLAT_PART')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GDLM60_PDM_STEEL_BRUSHED_54_SMOOTH_PART')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GLASS___BLUE_FLAT_PART')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'OBJ_F1_FOUNDATIONS_GLASS___BLUE_SMOOTH_PART')
+            modelHouse[i].visibility = false;
+        else if(modelName == 'BEAM_F1_FOUNDATIONS_WOOD___HARDWOOD')
+            modelHouse[i].visibility = false;
 
         if(modelName.includes("PLAN_FLOOR_02"))
             modelHouse[i].visibility = true;
@@ -582,32 +607,10 @@ function onPointerMove(evt){
     })
     if(pickInfo.hit){
         var currentMesh = pickInfo.pickedMesh;
-        var isPlan = false;
-        for(var i in planList){
-            if(currentMesh.visibility && currentMesh.name == planList[i].name){
-                isPlan = true;
-                break;
-            }
-        }
-        if(isPlan){
-            if(current !== currentMesh){
-                current = currentMesh;
-            }
-            else return;
-
-            loadPlan();
-            var random = Math.floor(Math.random()*10)%3 + 1;
-            currentMesh.material = new BABYLON.StandardMaterial(currentMesh.name+random, scene);
-            var baseUrl;
-            if(currentMesh.name.includes("PLAN_FLOOR_02"))
-                baseUrl = "assets/texture/plans/floor-plan-2-random-color-";
-            else
-                baseUrl = "assets/texture/plans/floor-plan-1-random-color-";
-            currentMesh.material.diffuseTexture = new BABYLON.Texture(baseUrl+random+".png", scene);
-            currentMesh.material.diffuseTexture.uScale = 0.0016;
-            currentMesh.material.diffuseTexture.vScale = 0.0016;
-            currentMesh.material.specularColor = new BABYLON.Color3(0, 0, 0);
-        }
+        hl.isEnabled = true;
+    }
+    else{
+        hl.isEnabled = false;
     }
 }
 
@@ -616,7 +619,7 @@ function showModal(target){
                 '<div class="modal-container">'+
                 '<div class="modal-content">'+
                 '<div class="close-modal"></div>'+
-                '<img src="assets/texture/plans/seperated-plans/'+target+'"></img>'+
+                '<img src="assets/texture/plans/'+target+'" height="100%"></img>'+
                 '</div>'+
                 '</div>'
             );
@@ -633,20 +636,13 @@ function onPointerDown(evt){
         var currentMesh = pickInfo.pickedMesh;
         console.log(currentMesh.name)
         //if plan
-        if(currentMesh.name.includes("PLAN_FLOOR_01") || currentMesh.name.includes("PLAN_FLOOR_02"))
+        if(currentMesh.name.includes("PLAN_FLOOR_01"))
         {
-            var isPlan = false;
-            var target = null;
-            for(var i in planList){
-                if(currentMesh.visibility && currentMesh.name == planList[i].name){
-                    isPlan = true;
-                    target = planList[i].target;
-                    break;
-                }
-            }
-            if(isPlan){
-                showModal(target);
-            }
+            showModal("floor-plan-1.png");
+        }
+        else if(currentMesh.name.includes("PLAN_FLOOR_02"))
+        {
+            showModal("floor-plan-2.png");
         }
         //else if sticker
         else if(currentMesh.name.includes('camera-sticker-')){
